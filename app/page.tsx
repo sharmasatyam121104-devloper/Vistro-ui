d07b17404c7da80d00cb3dfa0c411a380d0b6b04
sharmasatyam121104-devloper <sharma.satyam121104@gmail.com>
2026-02-28 20:07:59 +0530

    feat: video-player added, but issues in quality changeing and subtitles

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import VideoPlayer from '@/components/shared/video-player'
+
+
 const HomeRouter = () => {
   return (
-    <div>HomeRouter</div>
+    <div className='w-8/12'>
+      <VideoPlayer 
+        src="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+      />
+    </div>
   )
 }
 
